6951547b48d7ea8d800f69073f803db7eba89efe
Yisui Hu <hu@crafting.dev>
2023-04-07 21:08:16 +0000

    update CLI to 1.3.21-ff7bf95

--- Formula/cs.rb
+++ Formula/cs.rb
@@ -1,9 +1,9 @@
 class Cs < Formula
     desc "CLI for Cloud Sandboxes"
     homepage "https://sandboxes.cloud"
-    url "https://storage.googleapis.com/cloud-sandboxes/cs/1.3.21-c3213c7/cs-darwin-amd64.tar.gz"
-    version "1.3.21-c3213c7"
-    sha256 "4786f2218840db25b44c29049d4a0cf3cf2cc64c7783956de1cf82104f71833b"
+    url "https://storage.googleapis.com/cloud-sandboxes/cs/1.3.21-ff7bf95/cs-darwin-amd64.tar.gz"
+    version "1.3.21-ff7bf95"
+    sha256 "f07460a70da39cd5ba17cd1e1fa43e17028d0c9aef10c406143543136368f8d6"
   
     def install
       bin.install "cs"
